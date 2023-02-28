a219a8cf0485b7a8b9bcc290c92c255369f28c76
PiLoT-P <59173880+PiLoT-P@users.noreply.github.com>
2023-02-28 19:08:50 +0200

    2 завдання готові

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
+import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const icon = galleryItems
+    .map(image => 
+        `<a class="gallery__item" href="${image.original}">
+            <img 
+            class="gallery__image" 
+            src="${image.preview}" 
+            alt="${image.description}" 
+            title="${image.description}"
+            />
+        </a>`)
+    .join('');
+gallery.insertAdjacentHTML('afterbegin', icon);
+
+const lightbox = new SimpleLightbox('.gallery a', {captionDelay: 250,});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from "@vimeo/player";
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+let timeSet = localStorage.getItem('videoplayer-current-time');
+
+const onPlay = function (data) {
+    console.log(data.seconds);
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+    // data is an object containing properties specific to that event
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(timeSet);
 
